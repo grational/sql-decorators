26324e13a25291adb39dbe64241b8404c5991694
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2017-09-29 12:56:25 +0200

    Update to add driverClassName to the hikari configuration

--- src/main/groovy/it/italiaonline/rnd/db/HikariConf.groovy
+++ src/main/groovy/it/italiaonline/rnd/db/HikariConf.groovy
@@ -4,12 +4,14 @@ import com.zaxxer.hikari.HikariConfig
 class HikariConf {
 
 	final HikariConfig conf
+	private final String  driverEnv
 	private final String  urlEnv
 	private final String  usernameEnv
 	private final String  passwordEnv
 	private final Integer cores = Runtime.runtime.availableProcessors()
 
 	HikariConf(Map params) {
+		this.driverEnv   = "${params.env_prefix}_DB_DRIVER"
 		this.urlEnv      = "${params.env_prefix}_DB_JDBC_URL"
 		this.usernameEnv = "${params.env_prefix}_DB_USERNAME"
 		this.passwordEnv = "${params.env_prefix}_DB_PASSWORD"
@@ -17,24 +19,24 @@ class HikariConf {
 		this.conf = new HikariConfig()
 		this.conf.with {
 			// authentication
-			jdbcUrl  = Objects.requireNonNull (
-				System.getenv()[urlEnv],
-				"'${urlEnv}' env variable is required"
-			)
-			username = Objects.requireNonNull (
-				System.getenv()[usernameEnv],
-				"'${usernameEnv}' env variable is required"
-			)
-			password = Objects.requireNonNull (
-				System.getenv()[passwordEnv],
-				"'${passwordEnv}' env variable is required"
-			)
+			driverClassName = this.getEnv(driverEnv)
+			jdbcUrl         = this.getEnv(urlEnv)
+			username        = this.getEnv(usernameEnv)
+			password        = this.getEnv(passwordEnv)
 			// performance tuning
 			maximumPoolSize = params.max_pool_size ?: (cores * 2)
 			minimumIdle     = params.min_idle ?: cores
+			// additional params
 			params?.datasource_properties?.each { property, value ->
 				addDataSourceProperty(property, value)
 			}
 		}
+	}
+
+	private String getEnv(String varName) {
+		Objects.requireNonNull (
+			System.getenv()[varName],
+			"'${varName}' env variable is required"
+		)
 	}
 }
