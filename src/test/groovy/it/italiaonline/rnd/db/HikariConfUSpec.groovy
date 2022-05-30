ab30af093d1ff6b3b2abe67f07edbd8454b0989d
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2022-05-30 20:14:44 +0200

    Updating the project to leverage groovy 3.x

--- src/test/groovy/it/italiaonline/rnd/db/HikariConfUSpec.groovy
+++ src/test/groovy/it/italiaonline/rnd/db/HikariConfUSpec.groovy
@@ -1,82 +1,78 @@
 package it.italiaonline.rnd.db
 
 import spock.lang.*
-import java.lang.reflect.Field
+import it.grational.specification.Environment
 
 class HikariConfUSpec extends Specification {
 
+	@Shared String prefix = 'PREFIX'
+
+	@Shared
+	Environment env = new Environment (
+		"${prefix}_DB_JDBC_URL": 'jdbc_url',
+		"${prefix}_DB_USERNAME": 'username',
+		"${prefix}_DB_PASSWORD": 'password',
+		"${prefix}_DB_DRIVER":   'com.mysql.cj.jdbc.Driver'
+	)
+
 	def 'Should return a NullPointerException if one of the required env vars is not defined'() {
 		when:
-			def hc = new HikariConf (env_prefix: 'PREFIX')
+			def hc = new HikariConf(env_prefix: prefix)
 		then:
 			def error = thrown(NullPointerException)
-			error.message == "'PREFIX_DB_JDBC_URL' env variable is required"
+			error.message == "'${prefix}_DB_JDBC_URL' env variable is required"
 	}
 
 	def 'Should correctly handle the set env variables'() {
 		given:
-			Map env = [
-				PREFIX_DB_JDBC_URL: 'jdbc_url',
-				PREFIX_DB_USERNAME: 'username',
-				PREFIX_DB_PASSWORD: 'password',
-				PREFIX_DB_DRIVER:   'com.mysql.cj.jdbc.Driver'
-			]
-		and:
-			setEnv(env)
+			env.insert()
+
 		when:
-			def db = new HikariConf(env_prefix: 'PREFIX').conf
+			def db = new HikariConf(env_prefix: prefix).conf
+
 		then:
 			noExceptionThrown()
 		and:
-			db.jdbcUrl         == env.PREFIX_DB_JDBC_URL
-			db.username        == env.PREFIX_DB_USERNAME
-			db.password        == env.PREFIX_DB_PASSWORD
-			db.driverClassName == env.PREFIX_DB_DRIVER
+			db.jdbcUrl         == System.getenv()."${prefix}_DB_JDBC_URL"
+			db.username        == System.getenv()."${prefix}_DB_USERNAME"
+			db.password        == System.getenv()."${prefix}_DB_PASSWORD"
+			db.driverClassName == System.getenv()."${prefix}_DB_DRIVER"
+
+		cleanup:
+			env.clean()
 	}
 
 	def 'Should correctly set all the Hikari configuration'() {
 		given:
-			def env = [
-				PREFIX_DB_JDBC_URL: 'jdbc_url',
-				PREFIX_DB_USERNAME: 'username',
-				PREFIX_DB_PASSWORD: 'password',
-				PREFIX_DB_DRIVER:   'com.mysql.cj.jdbc.Driver'
-			]
-		and:
 			def cores = Runtime.runtime.availableProcessors()
-		and:
-			def prefix = 'PREFIX'
 		and:
 			def dataSourceProperties = [
 				useSSL: 'false',
 				tinyInt1isBit: 'false'
 			]
-		when:
-			setEnv(env)
 		and:
+			env.insert()
+		
+		when:
 			def hikari = new HikariConf (
 				env_prefix:            prefix,
 				datasource_properties: dataSourceProperties
 			)
+
 		then:
 			hikari.conf.with {
-				jdbcUrl                              == env.PREFIX_DB_JDBC_URL
-				username                             == env.PREFIX_DB_USERNAME
-				password                             == env.PREFIX_DB_PASSWORD
-				driverClassName                      == env.PREFIX_DB_DRIVER
+				jdbcUrl                              == System.getenv()."${prefix}_DB_JDBC_URL"
+				username                             == System.getenv()."${prefix}_DB_USERNAME"
+				password                             == System.getenv()."${prefix}_DB_PASSWORD"
+				driverClassName                      == System.getenv()."${prefix}_DB_DRIVER"
 				maximumPoolSize                      == (cores * 2)
 				minimumIdle                          == cores
 				dataSourceProperties.'useSSL'        == 'false'
 				dataSourceProperties.'tinyInt1isBit' == 'false'
 			}
+
+		cleanup:
+			env.clean()
 	}
 
-	void setEnv(Map envVars) {
-		Map immutableEnv = System.getenv()
-		Class<?> cl      = immutableEnv.getClass()
-		Field field      = cl.getDeclaredField("m")
-		field.accessible = true
-		Map mutableEnv   = field.get(immutableEnv)
-		mutableEnv << envVars
-	}
 }
